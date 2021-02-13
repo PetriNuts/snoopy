1d7d9d4876999eb24f4df6d8909a2aee4ba591aa
george-btu <engdotgeorge@gmail.com>
2021-02-13 20:35:50 +0100

    add SP Vector 1D and 2D of int to  SP Data struture defintions

--- sp_core/SP_Vector.h
+++ sp_core/SP_Vector.h
@@ -21,12 +21,14 @@ typedef vector<long> SP_VectorLong;
 typedef vector<unsigned long> SP_VectorULong;
 typedef vector<bool> SP_VectorBool;
 typedef vector<short> SP_VectorShort;
+typedef vector<int> SP_VectorInt;//george2021
 
 typedef vector< SP_VectorString > SP_Vector2DString;
 typedef vector< SP_VectorDouble > SP_Vector2DDouble;
 typedef vector< SP_VectorLong > SP_Vector2DLong;
 typedef vector< SP_VectorULong > SP_Vector2DULong;
 typedef vector< SP_VectorBool > SP_Vector2DBool;
+typedef vector< SP_VectorInt > SP_Vector2DInt;//george2021
 struct TraceElement {//Added by G.A
 	std::vector<double> sample;
 	double currentLevel;
