74248fffdb7a76435d9d11010364d46ec565cd0a
George Assaf <engdotgeorge@gmail.com>
2022-12-17 10:50:40 +0100

    update snoopy revision

--- sp_revision.cpp
+++ sp_revision.cpp
@@ -3,7 +3,7 @@
 const wxString SP_APP_SHORT_NAME  = wxT("Snoopy");
 const wxString SP_APP_VERSION     = wxT("2");
 const wxString SP_APP_LONG_NAME   = SP_APP_SHORT_NAME + wxT(" ") + SP_APP_VERSION;
-const wxString SP_APP_REVISION    = wxT("2.0");
+const wxString SP_APP_REVISION    = wxT("2.5");
 const wxString SP_APP_RELEASE     = wxT("beta");
 const wxString SP_APP_VENDOR      = wxT("Brandenburg University of Technology Cottbus\nData Structures and Software Dependability Group");
 const wxString SP_APP_WEBPAGE	  =	wxT("http://www-dssz.informatik.tu-cottbus.de/software/snoopy.html");
